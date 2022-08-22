d1eac9f8065eed46ebf5038e74e5e98dae36a589
DamianPalavecino <damianpalavecino25@gmail.com>
2022-08-22 18:13:24 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const fetch = (url) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(url));
-
-fetch("https://fakestoreapi.com/products")
-  .then((res) => res.json())
-  .then((json) => console.log(json));
