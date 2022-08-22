eb217e74deb34377d82468cf741b304e9bdad7b3
DamianPalavecino <damianpalavecino25@gmail.com>
2022-08-22 19:39:51 -0300

    Test new routes

--- router.js
+++ router.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const productsController = require("./controllers/productController");
+const cartsController = require("./controllers/cartController");
+const usersController = require("./controllers/userController");
 
 router.get("/", (req, res) => {
   res.send("Root");
 });
 
 router.get("/products", productsController.getAllProducts);
+router.get("/carts", cartsController.getAllCarts);
+router.get("/users", usersController.getAllUsers);
 
 module.exports = router;
