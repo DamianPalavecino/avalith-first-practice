86528788edab2be04e35c3253af889b7fd0af275
DamianPalavecino <damianpalavecino25@gmail.com>
2022-08-22 18:13:47 -0300

    Set up server in port 3000

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const router = require("./router");
 const app = express();
 const port = 3000;
-const router = express.Router();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use("/", router);
+
+app.listen(port, () => {
+  console.log("App is running on port " + port);
 });
-
-app.listen(port);
